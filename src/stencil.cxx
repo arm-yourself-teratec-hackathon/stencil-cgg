929f5f97401771a04ca1082f200e412859b26c0e
dssgabriel <dss.gabriel@protonmail.com>
2022-11-29 00:43:25 +0100

    C++-ified the code\
    - replaced C-style arrays with `std::vector`;
    - changed function declarations to C++11 ones, with forward-declarations;
    - translated comments and documentation;
    - added some magic numbers are `constexpr`;
    - misc code clean up.

--- src/stencil.cxx
+++ src/stencil.cxx
@@ -1,90 +1,96 @@
 #include <assert.h>
+#include <cstdint>
 #include <iostream>
 #include <math.h>
 #include <omp.h>
 #include <sys/time.h>
+#include <vector>
 
-double dml_micros() {
-    static struct timezone tz;
+[[nodiscard]] auto main(int32_t argc, char** argv) -> int32_t;
+auto init() -> void;
+auto one_iteration() -> void;
+[[nodiscard]] auto DIMXYZ(uint64_t x, uint64_t y, uint64_t z) -> uint64_t;
+[[nodiscard]] auto MATXYZ(uint64_t x, uint64_t y, uint64_t z) -> uint64_t;
+[[nodiscard]] auto dml_micros() -> double;
+
+// Constant expressions declarations
+constexpr uint64_t order = 8;
+constexpr double ONE_MILLION = 1000000.0;
+
+// Globals declarations
+uint64_t DIMX, DIMY, DIMZ, iters;
+uint64_t MAXX, MAXY, MAXZ;
+uint64_t xyplane, MATsize;
+std::vector<double> matA;
+std::vector<double> matB;
+std::vector<double> matC;
+
+/// Get current time in microseconds.
+[[nodiscard]] auto dml_micros() -> double {
     static struct timeval tv;
+    static struct timezone tz;
     gettimeofday(&tv, &tz);
-    return ((tv.tv_sec * 1000000.0) + tv.tv_usec);
+    return tv.tv_sec * ONE_MILLION + tv.tv_usec;
 }
 
-using ui64 = unsigned long long;
-
-constexpr ui64 order = 8;
-ui64 DIMX, DIMY, DIMZ, iters;
-ui64 MAXX, MAXY, MAXZ;
-ui64 xyplane, MATsize;
-
-// retourne un offset dans le centre de la matrice les dimensions sont [0..DIM-1]
-inline ui64 DIMXYZ(ui64 x, ui64 y, ui64 z) {
+/// Returns an offset in the center of a matrix of linear dimensions [0:DIM-1]
+[[nodiscard]] auto DIMXYZ(uint64_t x, uint64_t y, uint64_t z) -> uint64_t {
     return ((z + order) * xyplane + (y + order) * MAXX + x + order);
 }
 
-// retourne un offset dans la matrice les dimensions sont [-order..DIM+order-1] mais en indices de
-// [0..DIM+2*order-1]
-inline ui64 MATXYZ(ui64 x, ui64 y, ui64 z) {
+/// Returns an offset in a matrix of linear dimensions [-order:DIM+order-1] but
+/// in indices of [0:DIM+order*2-1]
+[[nodiscard]] auto MATXYZ(uint64_t x, uint64_t y, uint64_t z) -> uint64_t {
     return (x + y * MAXX + z * xyplane);
 }
 
-double* matA;
-double* matB;
-double* matC;
+/// The initialization isn't part of the exercise but can be optimized nonetheless.
+/// It is however not accounted for in the performance evaluation as it artificially
+/// fills the matrices with constant data and does not influence performance.
+///
+/// The A and C matrices are initialized to zero, with A the input and C the output.
+auto init() -> void {
+    // Dynamically allocate memory of size DIMX * DIMY * DIMZ + ghost region on 6 faces
+    matA = std::vector<double>(MATsize, 0.0);
+    matB = std::vector<double>(MATsize, 0.0);
+    matC = std::vector<double>(MATsize, 0.0);
 
-void init() {
-    // l initialisation ne fait pas partie de l exercise , elle peut etre optimisee mais n est pas
-    // mesuree car elle remplie de facon artificielle les matrices les donnees n influent pas sur la
-    // performance
-
-    // dynamically allocate memory of size DIMX*DIMY*DIMZ+ghost region on 6 faces
-    matA = new double[MATsize];
-    assert(matA != NULL);
-    matB = new double[MATsize];
-    assert(matB != NULL);
-    matC = new double[MATsize];
-    assert(matC != NULL);
-
-    // Initialisation centre et bords
-    // Les matrices A et C sont mises a zero
-    // A en la matrice d emtree et C la matrice de sortie
-    // La matrice B est un stencil constant pour le run
-    for (ui64 z = 0; z < MAXZ; z++) {
-        for (ui64 y = 0; y < MAXY; y++) {
-            for (ui64 x = 0; x < MAXX; x++) {
-                matA[MATXYZ(x, y, z)] = 0.0;
-                matC[MATXYZ(x, y, z)] = 0.0;
-                matB[MATXYZ(x, y, z)] = sin(z * cos(x + 0.311) * cos(y + .817) + .613);
+    // Center and edges initialization, B is constant stencil for the run
+    for (uint64_t z = 0; z < MAXZ; ++z) {
+        for (uint64_t y = 0; y < MAXY; ++y) {
+            for (uint64_t x = 0; x < MAXX; ++x) {
+                matB[MATXYZ(x, y, z)] = sin(z * cos(x + 0.311) * cos(y + 0.817) + 0.613);
             }
         }
     }
-    // Initialisation centre de A qui est la matrice de data
-    for (ui64 z = 0; z < DIMZ; z++) {
-        for (ui64 y = 0; y < DIMY; y++) {
-            for (ui64 x = 0; x < DIMX; x++) {
+
+    // Initialize the center of A, which is the data matrix
+    for (uint64_t z = 0; z < DIMZ; ++z) {
+        for (uint64_t y = 0; y < DIMY; ++y) {
+            for (uint64_t x = 0; x < DIMX; ++x) {
                 matA[DIMXYZ(x, y, z)] = 1.0;
             }
         }
     }
 }
 
-void one_iteration() {
+auto one_iteration() -> void {
     #pragma omp parallel for collapse(2) schedule(guided)
-    for (ui64 z = 0; z < DIMZ; z++) {
-        for (ui64 y = 0; y < DIMY; y++) {
+    for (uint64_t z = 0; z < DIMZ; ++z) {
+        for (uint64_t y = 0; y < DIMY; ++y) {
             #pragma omp simd
-            for (ui64 x = 0; x < DIMX; x++) {
-                ui64 xyz = DIMXYZ(x, y, z);
+            for (uint64_t x = 0; x < DIMX; ++x) {
+                uint64_t xyz = DIMXYZ(x, y, z);
                 matC[xyz] = matA[xyz] * matB[xyz];
-                for (ui64 o = 1; o <= order; o++) {
+
+                for (uint64_t o = 1; o <= order; ++o) {
                     const double exponent = 1.0 / pow(17.0, o);
-                    const ui64 xpo_yz = DIMXYZ(x + o, y, z);
-                    const ui64 xmo_yz = DIMXYZ(x - o, y, z);
-                    const ui64 x_ypo_z = DIMXYZ(x, y + o, z);
-                    const ui64 x_ymo_z = DIMXYZ(x, y - o, z);
-                    const ui64 xy_zpo = DIMXYZ(x, y, z + o);
-                    const ui64 xy_zmo = DIMXYZ(x, y, z - o);
+                    const uint64_t xpo_yz = DIMXYZ(x + o, y, z);
+                    const uint64_t xmo_yz = DIMXYZ(x - o, y, z);
+                    const uint64_t x_ypo_z = DIMXYZ(x, y + o, z);
+                    const uint64_t x_ymo_z = DIMXYZ(x, y - o, z);
+                    const uint64_t xy_zpo = DIMXYZ(x, y, z + o);
+                    const uint64_t xy_zmo = DIMXYZ(x, y, z - o);
 
                     matC[xyz] += matA[xpo_yz] * matB[xpo_yz] * exponent;
                     matC[xyz] += matA[xmo_yz] * matB[xmo_yz] * exponent;
@@ -99,18 +105,18 @@ void one_iteration() {
 
     // A = C
     #pragma omp parallel for collapse(2) schedule(guided)
-    for (ui64 z = 0; z < DIMZ; z++) {
-        for (ui64 y = 0; y < DIMY; y++) {
+    for (uint64_t z = 0; z < DIMZ; ++z) {
+        for (uint64_t y = 0; y < DIMY; ++y) {
             #pragma omp simd
-            for (ui64 x = 0; x < DIMX; x++) {
-                ui64 xyz = DIMXYZ(x, y, z);
+            for (uint64_t x = 0; x < DIMX; ++x) {
+                uint64_t xyz = DIMXYZ(x, y, z);
                 matA[xyz] = matC[xyz];
             }
         }
     }
 }
 
-int main(const int argc, char** argv) {
+[[nodiscard]] auto main(int32_t argc, char** argv) -> int32_t {
     try {
         DIMX = std::stoi(argv[1]);
         DIMY = std::stoi(argv[2]);
@@ -121,33 +127,26 @@ int main(const int argc, char** argv) {
         MAXZ = DIMZ + 2 * order;
         xyplane = MAXX * MAXY;
         MATsize = MAXX * MAXY * MAXZ;
-    }
-
-    catch (...) {
+    } catch (...) {
         std::cout << argv[0] << " siseX sizeY sizeZ iters" << std::endl;
         return -1;
     }
 
     init();
 
-    // phase1
-    for (ui64 i = 0; i < iters; i++) {
-        // calcule 1 iteration Jacobi C=B@A
+    for (uint64_t i = 0; i < iters; ++i) {
+        // Compute one iteration of Jacobi: C = B@A
         double t1 = dml_micros();
         one_iteration();
         double t2 = dml_micros();
 
-        printf("_0_");
-        for (ui64 j = 0; j < 5; j++) {
+        printf("_0_ ");
+        for (uint64_t j = 0; j < 5; ++j) {
             printf("%18.15lf ", matA[DIMXYZ(DIMX / 2 + j, DIMY / 2 + j, DIMZ / 2 + j)]);
         }
         double ns_point = (t2 - t1) * 1000.0 / DIMX / DIMY / DIMZ;
-        printf("\033[1m%10.0lf\033[0m %10.3lf %llu %llu %llu\n", t2 - t1, ns_point, DIMX, DIMY, DIMZ);
+        printf("\033[1m%10.0lf\033[0m %10.3lf %lu %lu %lu\n", t2 - t1, ns_point, DIMX, DIMY, DIMZ);
     }
-
-    delete[] matA;
-    delete[] matB;
-    delete[] matC;
 
     return 0;
 }
