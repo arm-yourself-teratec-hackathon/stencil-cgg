ca54f2f721db1a560c0400b0cbab9c130dbda2cc
dssgabriel <dss.gabriel@protonmail.com>
2022-11-28 11:25:19 +0100

    Factorized array access indices + some code cleanup

--- stencil.cxx
+++ stencil.cxx
@@ -9,11 +9,10 @@ double dml_micros() {
     gettimeofday(&tv, &tz);
     return ((tv.tv_sec * 1000000.0) + tv.tv_usec);
 }
-using namespace std;
 
-typedef unsigned long long ui64;
+using ui64 = unsigned long long;
 
-const ui64 order = 8;
+constexpr ui64 order = 8;
 ui64 DIMX, DIMY, DIMZ, iters;
 ui64 MAXX, MAXY, MAXZ;
 ui64 xyplane, MATsize;
@@ -73,20 +72,23 @@ void one_iteration() {
     for (ui64 z = 0; z < DIMZ; z++) {
         for (ui64 y = 0; y < DIMY; y++) {
             for (ui64 x = 0; x < DIMX; x++) {
-                matC[DIMXYZ(x, y, z)] = matA[DIMXYZ(x, y, z)] * matB[DIMXYZ(x, y, z)];
+                ui64 xyz = DIMXYZ(x, y, z);
+                matC[xyz] = matA[xyz] * matB[xyz];
                 for (ui64 o = 1; o <= order; o++) {
-                    matC[DIMXYZ(x, y, z)] +=
-                        matA[DIMXYZ(x + o, y, z)] * matB[DIMXYZ(x + o, y, z)] / pow(17.0, o);
-                    matC[DIMXYZ(x, y, z)] +=
-                        matA[DIMXYZ(x - o, y, z)] * matB[DIMXYZ(x - o, y, z)] / pow(17.0, o);
-                    matC[DIMXYZ(x, y, z)] +=
-                        matA[DIMXYZ(x, y + o, z)] * matB[DIMXYZ(x, y + o, z)] / pow(17.0, o);
-                    matC[DIMXYZ(x, y, z)] +=
-                        matA[DIMXYZ(x, y - o, z)] * matB[DIMXYZ(x, y - o, z)] / pow(17.0, o);
-                    matC[DIMXYZ(x, y, z)] +=
-                        matA[DIMXYZ(x, y, z + o)] * matB[DIMXYZ(x, y, z + o)] / pow(17.0, o);
-                    matC[DIMXYZ(x, y, z)] +=
-                        matA[DIMXYZ(x, y, z - o)] * matB[DIMXYZ(x, y, z - o)] / pow(17.0, o);
+                    const double exponent = pow(17.0, o);
+                    const ui64 xpo_yz = DIMXYZ(x + o, y, z);
+                    const ui64 xmo_yz = DIMXYZ(x - o, y, z);
+                    const ui64 x_ypo_z = DIMXYZ(x, y + o, z);
+                    const ui64 x_ymo_z = DIMXYZ(x, y - o, z);
+                    const ui64 xy_zpo = DIMXYZ(x, y, z + o);
+                    const ui64 xy_zmo = DIMXYZ(x, y, z - o);
+
+                    matC[xyz] += matA[xpo_yz] * matB[xpo_yz] / exponent;
+                    matC[xyz] += matA[xmo_yz] * matB[xmo_yz] / exponent;
+                    matC[xyz] += matA[x_ypo_z] * matB[x_ypo_z] / exponent;
+                    matC[xyz] += matA[x_ymo_z] * matB[x_ymo_z] / exponent;
+                    matC[xyz] += matA[xy_zpo] * matB[xy_zpo] / exponent;
+                    matC[xyz] += matA[xy_zmo] * matB[xy_zmo] / exponent;
                 }
             }
         }
@@ -127,11 +129,13 @@ int main(const int argc, char** argv) {
         double t1 = dml_micros();
         one_iteration();
         double t2 = dml_micros();
-        printf("_0_ ");
-        for (ui64 i = 0; i < 5; i++)
-            printf(" %18.15lf", matA[DIMXYZ(DIMX / 2 + i, DIMY / 2 + i, DIMZ / 2 + i)]);
+
+        printf("_0_");
+        for (ui64 j = 0; j < 5; j++) {
+            printf("%18.15lf ", matA[DIMXYZ(DIMX / 2 + j, DIMY / 2 + j, DIMZ / 2 + j)]);
+        }
         double ns_point = (t2 - t1) * 1000.0 / DIMX / DIMY / DIMZ;
-        printf("  %10.0lf  %10.3lf %lld %lld %lld\n", t2 - t1, ns_point, DIMX, DIMY, DIMZ);
+        printf("\033[1m%10.0lf\033[0m %10.3lf %llu %llu %llu\n", t2 - t1, ns_point, DIMX, DIMY, DIMZ);
     }
 
     delete[] matA;
