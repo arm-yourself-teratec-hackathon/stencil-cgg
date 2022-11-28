f2a1fa0631ef8318738290ce0b5dc7ab04329f31
dssgabriel <dss.gabriel@protonmail.com>
2022-11-28 15:02:02 +0100

    Enhanced speedup script Now checks that output values are equal to reference values. Added color

--- scripts/speedup.py
+++ scripts/speedup.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 
+import math
 import os
+import sys
 
 
+# Strip color/formating on strings
 def colorstrip(data: str) -> str:
     find = data.find('\x03')
     while find > -1:
@@ -36,6 +39,7 @@ def colorstrip(data: str) -> str:
             except: pass
 
         find = data.find('\x03')
+
     data = data.replace('\x1b[1m','')
     data = data.replace('\x1b[0m','')
     data = data.replace('\x02','')
@@ -43,7 +47,9 @@ def colorstrip(data: str) -> str:
     data = data.replace('\x1f','')
     data = data.replace('\x16','')
     data = data.replace('\x0f','')
+
     return data
+
 
 def run(command: str) -> list:
     run = os.popen(command)
@@ -54,18 +60,40 @@ def run(command: str) -> list:
 
 
 def main():
-    ref = run("ref/stencil-x86 100 100 100 5")
-    cur = run("./stencil 100 100 100 5")
+    dim_x = 100
+    dim_y = 100
+    dim_z = 100
+    iter = 5
+    
+    # Allow user to override default parameters
+    if len(sys.argv) == 5:
+        dim_x = sys.argv[1]
+        dim_y = sys.argv[2]
+        dim_z = sys.argv[3]
+        iter = sys.argv[4]
+    elif len(sys.argv) != 1:
+        print("\033[1mUsage:\033[0m <dim_x> <dim_y> <dim_z> <iter>")
+        exit(-1)
+
+    ref = run(f"ref/stencil-x86 {dim_x} {dim_y} {dim_z} {iter}")
+    cur = run(f"./stencil {dim_x} {dim_y} {dim_z} {iter}")
     ref_ms = []
     cur_ms = []
     for i, (r, c) in enumerate(zip(ref, cur)):
-        r = r.split()
-        c = c.split()
+        r = [float(x) for x in r.split()[1:]]
+        c = [float(x) for x in c.split()[1:]]
         
-        # assert c[1] == r[1] and c[2] == r[2] and c[3] == r[3], "error: coefficients aren't coherent"
-        ref_ms.append(float(r[6]))
-        cur_ms.append(float(c[6]))
+        # Assert that result of the current version is still valid compared to reference
+        for j in range(1, 5):
+            if not math.isclose(c[j], r[j]):
+                print(f"\033[1;31merror:\033[0m coefficients at iteration \033[1m#{i}\033[0m are incoherent.\n",
+                      f"   -> reference is \033[34m{r[j]}\033[0m, but current is \033[34m{c[j]}\033[0m")
+                exit(-1)
 
+        ref_ms.append(r[6])
+        cur_ms.append(c[6])
+
+    # Compute average
     ref_avg = 0.0
     cur_avg = 0.0
     for r, c in zip(ref_ms, cur_ms):
@@ -74,9 +102,9 @@ def main():
     ref_avg /= 5
     cur_avg /= 5
     
-    print(f"Average ref: {ref_avg} ms")
-    print(f"Average cur: {cur_avg} ms")
-    print(f"  Speedup: {ref_avg / cur_avg:.2f}")
+    print(f"Reference average: {ref_avg:03.2f} us")
+    print(f"  Current average: {cur_avg:03.2f} us")
+    print(f"\nSpeedup: \033[1;32m{ref_avg / cur_avg:.2f}\033[0m")
 
 
 if __name__ == "__main__":
