8e15c3acc9371bf840a17b36a1c2f1e365aa616b
fm16191 <56755856+fm16191@users.noreply.github.com>
2022-12-02 01:08:27 +0100

    Stencil : Loop blocking
    It seems that loop blocking is less efficient than using the `pragma omp simd` instruction along in the `parallel for schedule(dynamic)` clause ...

--- src/stencil.cxx
+++ src/stencil.cxx
@@ -28,6 +28,9 @@ auto one_iteration() -> void;
 #ifndef DIMZ
 #    define DIMZ 500UL
 #endif
+#ifndef BLOCK_SIZE
+#    define BLOCK_SIZE 20UL
+#endif
 
 // Constant expressions declarations
 constexpr double ONE_MILLION = 1000000.0;
@@ -37,7 +40,7 @@ constexpr uint64_t MAXY = DIMY + 2 * order;
 constexpr uint64_t MAXZ = DIMZ + 2 * order;
 constexpr uint64_t xyplane = MAXX * MAXY;
 constexpr uint64_t MATsize = MAXX * MAXY * MAXZ;
-constexpr uint64_t BLOCK_SIZE = 20;
+// constexpr uint64_t BLOCK_SIZE = 20;
 
 // Global variables declarations
 // Dynamically allocate memory of size DIMX * DIMY * DIMZ + ghost region on 6 faces
@@ -97,6 +100,25 @@ auto init() -> void {
 }
 
 auto one_iteration() -> void {
+
+    // #pragma omp parallel for schedule(dynamic)
+    // for (uint64_t bz = 0; bz < DIMZ; bz += BLOCK_SIZE) {
+    //     for (uint64_t by = 0; by < DIMY; by += BLOCK_SIZE) {
+    //         for (uint64_t bx = 0; bx < DIMX; bx += BLOCK_SIZE) {
+    //             for (uint64_t z = bz; z < bz + BLOCK_SIZE; ++z) {
+
+    //                 for (uint64_t y = by; y < by + BLOCK_SIZE; ++y) {
+    //                     // #pragma omp simd
+    //                     for (uint64_t x = bx; x < bx + BLOCK_SIZE; ++x) {
+    //                         const uint64_t xyz = DIMXYZ(x, y, z);
+    //                         matAB[xyz] = matA[xyz] * matB[xyz];
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+    
     #pragma omp parallel for schedule(dynamic)
     for (uint64_t z = 0; z < DIMZ; ++z) {
         for (uint64_t y = 0; y < DIMY; ++y) {
@@ -109,90 +131,186 @@ auto one_iteration() -> void {
     }
 
     #pragma omp parallel for schedule(dynamic)
-    for (uint64_t z = 0; z < DIMZ; ++z) {
-        for (uint64_t y = 0; y < DIMY; ++y) {
-            #pragma omp simd
-            for (uint64_t x = 0; x < DIMX; ++x) {
-                // Pre-compute planes
-                const uint64_t xyz = DIMXYZ(x, y, z);
-                const uint64_t yz = (z + order) * xyplane + (y + order) * MAXX + order;
-                const uint64_t xz = (z + order) * xyplane + x + order;
-                const uint64_t xy = (y + order) * MAXX + x + order;
+    for (uint64_t bz = 0; bz < DIMZ; bz += 8) {
+        for (uint64_t by = 0; by < DIMY; by += 8) {
+            for (uint64_t bx = 0; bx < DIMX; bx += 20) {
+                // #pragma omp simd
+                for (uint64_t z = bz; z < bz + 8; ++z) {
+                    for (uint64_t y = by; y < by + 8; ++y) {
+                        for (uint64_t x = bx; x < bx + 20; ++x) {
+                            // Pre-compute planes
+                            const uint64_t xyz = DIMXYZ(x, y, z);
+                            const uint64_t yz = (z + order) * xyplane + (y + order) * MAXX + order;
+                            const uint64_t xz = (z + order) * xyplane + x + order;
+                            const uint64_t xy = (y + order) * MAXX + x + order;
 
-                // Ensure compiler pre-loads exponents with `ld1rd` in SVE regs only once
-                const double exp0 = exponents[0];
-                const double exp1 = exponents[1];
-                const double exp2 = exponents[2];
-                const double exp3 = exponents[3];
-                const double exp4 = exponents[4];
-                const double exp5 = exponents[5];
-                const double exp6 = exponents[6];
-                const double exp7 = exponents[7];
+                            // Ensure compiler pre-loads exponents with `ld1rd` in SVE regs only
+                            // once
+                            const double exp0 = exponents[0];
+                            const double exp1 = exponents[1];
+                            const double exp2 = exponents[2];
+                            const double exp3 = exponents[3];
+                            const double exp4 = exponents[4];
+                            const double exp5 = exponents[5];
+                            const double exp6 = exponents[6];
+                            const double exp7 = exponents[7];
 
-                // Get `matC[xyz]` into temporary
-                double matC_xyz = matC[xyz];
+                            // Get `matC[xyz]` into temporary
+                            double matC_xyz = matC[xyz];
 
-                // Compute for current cell (o = 0)
-                matC_xyz = matAB[xyz];
+                            // Compute for current cell (o = 0)
+                            matC_xyz = matAB[xyz];
 
-                // Compute all orders on the x axis (first positive direction, then negative one)
-                matC_xyz += matAB[x + 1 + yz] * exp0;
-                matC_xyz += matAB[x + 2 + yz] * exp1;
-                matC_xyz += matAB[x + 3 + yz] * exp2;
-                matC_xyz += matAB[x + 4 + yz] * exp3;
-                matC_xyz += matAB[x + 5 + yz] * exp4;
-                matC_xyz += matAB[x + 6 + yz] * exp5;
-                matC_xyz += matAB[x + 7 + yz] * exp6;
-                matC_xyz += matAB[x + 8 + yz] * exp7;
-                matC_xyz += matAB[x - 1 + yz] * exp0;
-                matC_xyz += matAB[x - 2 + yz] * exp1;
-                matC_xyz += matAB[x - 3 + yz] * exp2;
-                matC_xyz += matAB[x - 4 + yz] * exp3;
-                matC_xyz += matAB[x - 5 + yz] * exp4;
-                matC_xyz += matAB[x - 6 + yz] * exp5;
-                matC_xyz += matAB[x - 7 + yz] * exp6;
-                matC_xyz += matAB[x - 8 + yz] * exp7;
+                            // Compute all orders on the x axis (first positive direction, then
+                            // negative one)
+                            matC_xyz += matAB[x + 1 + yz] * exp0;
+                            matC_xyz += matAB[x + 2 + yz] * exp1;
+                            matC_xyz += matAB[x + 3 + yz] * exp2;
+                            matC_xyz += matAB[x + 4 + yz] * exp3;
+                            matC_xyz += matAB[x + 5 + yz] * exp4;
+                            matC_xyz += matAB[x + 6 + yz] * exp5;
+                            matC_xyz += matAB[x + 7 + yz] * exp6;
+                            matC_xyz += matAB[x + 8 + yz] * exp7;
+                            matC_xyz += matAB[x - 1 + yz] * exp0;
+                            matC_xyz += matAB[x - 2 + yz] * exp1;
+                            matC_xyz += matAB[x - 3 + yz] * exp2;
+                            matC_xyz += matAB[x - 4 + yz] * exp3;
+                            matC_xyz += matAB[x - 5 + yz] * exp4;
+                            matC_xyz += matAB[x - 6 + yz] * exp5;
+                            matC_xyz += matAB[x - 7 + yz] * exp6;
+                            matC_xyz += matAB[x - 8 + yz] * exp7;
 
-                // Compute all orders on the y axis (first positive direction, then negative one)
-                matC_xyz += matAB[((y + 1 + order) * MAXX) + xz] * exp0;
-                matC_xyz += matAB[((y + 2 + order) * MAXX) + xz] * exp1;
-                matC_xyz += matAB[((y + 3 + order) * MAXX) + xz] * exp2;
-                matC_xyz += matAB[((y + 4 + order) * MAXX) + xz] * exp3;
-                matC_xyz += matAB[((y + 5 + order) * MAXX) + xz] * exp4;
-                matC_xyz += matAB[((y + 6 + order) * MAXX) + xz] * exp5;
-                matC_xyz += matAB[((y + 7 + order) * MAXX) + xz] * exp6;
-                matC_xyz += matAB[((y + 8 + order) * MAXX) + xz] * exp7;
-                matC_xyz += matAB[((y - 1 + order) * MAXX) + xz] * exp0;
-                matC_xyz += matAB[((y - 2 + order) * MAXX) + xz] * exp1;
-                matC_xyz += matAB[((y - 3 + order) * MAXX) + xz] * exp2;
-                matC_xyz += matAB[((y - 4 + order) * MAXX) + xz] * exp3;
-                matC_xyz += matAB[((y - 5 + order) * MAXX) + xz] * exp4;
-                matC_xyz += matAB[((y - 6 + order) * MAXX) + xz] * exp5;
-                matC_xyz += matAB[((y - 7 + order) * MAXX) + xz] * exp6;
-                matC_xyz += matAB[((y - 8 + order) * MAXX) + xz] * exp7;
+                            // Compute all orders on the y axis (first positive direction, then
+                            // negative one)
+                            matC_xyz += matAB[((y + 1 + order) * MAXX) + xz] * exp0;
+                            matC_xyz += matAB[((y + 2 + order) * MAXX) + xz] * exp1;
+                            matC_xyz += matAB[((y + 3 + order) * MAXX) + xz] * exp2;
+                            matC_xyz += matAB[((y + 4 + order) * MAXX) + xz] * exp3;
+                            matC_xyz += matAB[((y + 5 + order) * MAXX) + xz] * exp4;
+                            matC_xyz += matAB[((y + 6 + order) * MAXX) + xz] * exp5;
+                            matC_xyz += matAB[((y + 7 + order) * MAXX) + xz] * exp6;
+                            matC_xyz += matAB[((y + 8 + order) * MAXX) + xz] * exp7;
+                            matC_xyz += matAB[((y - 1 + order) * MAXX) + xz] * exp0;
+                            matC_xyz += matAB[((y - 2 + order) * MAXX) + xz] * exp1;
+                            matC_xyz += matAB[((y - 3 + order) * MAXX) + xz] * exp2;
+                            matC_xyz += matAB[((y - 4 + order) * MAXX) + xz] * exp3;
+                            matC_xyz += matAB[((y - 5 + order) * MAXX) + xz] * exp4;
+                            matC_xyz += matAB[((y - 6 + order) * MAXX) + xz] * exp5;
+                            matC_xyz += matAB[((y - 7 + order) * MAXX) + xz] * exp6;
+                            matC_xyz += matAB[((y - 8 + order) * MAXX) + xz] * exp7;
 
-                // Compute all orders on the z axis (first positive direction, then negative one)
-                matC_xyz += matAB[((z + 1 + order) * xyplane) + xy] * exp0;
-                matC_xyz += matAB[((z + 2 + order) * xyplane) + xy] * exp1;
-                matC_xyz += matAB[((z + 3 + order) * xyplane) + xy] * exp2;
-                matC_xyz += matAB[((z + 4 + order) * xyplane) + xy] * exp3;
-                matC_xyz += matAB[((z + 5 + order) * xyplane) + xy] * exp4;
-                matC_xyz += matAB[((z + 6 + order) * xyplane) + xy] * exp5;
-                matC_xyz += matAB[((z + 7 + order) * xyplane) + xy] * exp6;
-                matC_xyz += matAB[((z + 8 + order) * xyplane) + xy] * exp7;
-                matC_xyz += matAB[((z - 1 + order) * xyplane) + xy] * exp0;
-                matC_xyz += matAB[((z - 2 + order) * xyplane) + xy] * exp1;
-                matC_xyz += matAB[((z - 3 + order) * xyplane) + xy] * exp2;
-                matC_xyz += matAB[((z - 4 + order) * xyplane) + xy] * exp3;
-                matC_xyz += matAB[((z - 5 + order) * xyplane) + xy] * exp4;
-                matC_xyz += matAB[((z - 6 + order) * xyplane) + xy] * exp5;
-                matC_xyz += matAB[((z - 7 + order) * xyplane) + xy] * exp6;
-                matC_xyz += matAB[((z - 8 + order) * xyplane) + xy] * exp7;
+                            // Compute all orders on the z axis (first positive direction, then
+                            // negative one)
+                            matC_xyz += matAB[((z + 1 + order) * xyplane) + xy] * exp0;
+                            matC_xyz += matAB[((z + 2 + order) * xyplane) + xy] * exp1;
+                            matC_xyz += matAB[((z + 3 + order) * xyplane) + xy] * exp2;
+                            matC_xyz += matAB[((z + 4 + order) * xyplane) + xy] * exp3;
+                            matC_xyz += matAB[((z + 5 + order) * xyplane) + xy] * exp4;
+                            matC_xyz += matAB[((z + 6 + order) * xyplane) + xy] * exp5;
+                            matC_xyz += matAB[((z + 7 + order) * xyplane) + xy] * exp6;
+                            matC_xyz += matAB[((z + 8 + order) * xyplane) + xy] * exp7;
+                            matC_xyz += matAB[((z - 1 + order) * xyplane) + xy] * exp0;
+                            matC_xyz += matAB[((z - 2 + order) * xyplane) + xy] * exp1;
+                            matC_xyz += matAB[((z - 3 + order) * xyplane) + xy] * exp2;
+                            matC_xyz += matAB[((z - 4 + order) * xyplane) + xy] * exp3;
+                            matC_xyz += matAB[((z - 5 + order) * xyplane) + xy] * exp4;
+                            matC_xyz += matAB[((z - 6 + order) * xyplane) + xy] * exp5;
+                            matC_xyz += matAB[((z - 7 + order) * xyplane) + xy] * exp6;
+                            matC_xyz += matAB[((z - 8 + order) * xyplane) + xy] * exp7;
 
-                matC[xyz] = matC_xyz;
+                            matC[xyz] = matC_xyz;
+                        }
+                    }
+                }
             }
         }
     }
+
+    // #pragma omp parallel for schedule(dynamic)
+    // for (uint64_t z = 0; z < DIMZ; ++z) {
+    //     for (uint64_t y = 0; y < DIMY; ++y) {
+    //         #pragma omp simd
+    //         for (uint64_t x = 0; x < DIMX; ++x) {
+    //             // Pre-compute planes
+    //             const uint64_t xyz = DIMXYZ(x, y, z);
+    //             const uint64_t yz = (z + order) * xyplane + (y + order) * MAXX + order;
+    //             const uint64_t xz = (z + order) * xyplane + x + order;
+    //             const uint64_t xy = (y + order) * MAXX + x + order;
+
+    //             // Ensure compiler pre-loads exponents with `ld1rd` in SVE regs only once
+    //             const double exp0 = exponents[0];
+    //             const double exp1 = exponents[1];
+    //             const double exp2 = exponents[2];
+    //             const double exp3 = exponents[3];
+    //             const double exp4 = exponents[4];
+    //             const double exp5 = exponents[5];
+    //             const double exp6 = exponents[6];
+    //             const double exp7 = exponents[7];
+
+    //             // Get `matC[xyz]` into temporary
+    //             double matC_xyz = matC[xyz];
+
+    //             // Compute for current cell (o = 0)
+    //             matC_xyz = matAB[xyz];
+
+    //             // Compute all orders on the x axis (first positive direction, then negative one)
+    //             matC_xyz += matAB[x + 1 + yz] * exp0;
+    //             matC_xyz += matAB[x + 2 + yz] * exp1;
+    //             matC_xyz += matAB[x + 3 + yz] * exp2;
+    //             matC_xyz += matAB[x + 4 + yz] * exp3;
+    //             matC_xyz += matAB[x + 5 + yz] * exp4;
+    //             matC_xyz += matAB[x + 6 + yz] * exp5;
+    //             matC_xyz += matAB[x + 7 + yz] * exp6;
+    //             matC_xyz += matAB[x + 8 + yz] * exp7;
+    //             matC_xyz += matAB[x - 1 + yz] * exp0;
+    //             matC_xyz += matAB[x - 2 + yz] * exp1;
+    //             matC_xyz += matAB[x - 3 + yz] * exp2;
+    //             matC_xyz += matAB[x - 4 + yz] * exp3;
+    //             matC_xyz += matAB[x - 5 + yz] * exp4;
+    //             matC_xyz += matAB[x - 6 + yz] * exp5;
+    //             matC_xyz += matAB[x - 7 + yz] * exp6;
+    //             matC_xyz += matAB[x - 8 + yz] * exp7;
+
+    //             // Compute all orders on the y axis (first positive direction, then negative one)
+    //             matC_xyz += matAB[((y + 1 + order) * MAXX) + xz] * exp0;
+    //             matC_xyz += matAB[((y + 2 + order) * MAXX) + xz] * exp1;
+    //             matC_xyz += matAB[((y + 3 + order) * MAXX) + xz] * exp2;
+    //             matC_xyz += matAB[((y + 4 + order) * MAXX) + xz] * exp3;
+    //             matC_xyz += matAB[((y + 5 + order) * MAXX) + xz] * exp4;
+    //             matC_xyz += matAB[((y + 6 + order) * MAXX) + xz] * exp5;
+    //             matC_xyz += matAB[((y + 7 + order) * MAXX) + xz] * exp6;
+    //             matC_xyz += matAB[((y + 8 + order) * MAXX) + xz] * exp7;
+    //             matC_xyz += matAB[((y - 1 + order) * MAXX) + xz] * exp0;
+    //             matC_xyz += matAB[((y - 2 + order) * MAXX) + xz] * exp1;
+    //             matC_xyz += matAB[((y - 3 + order) * MAXX) + xz] * exp2;
+    //             matC_xyz += matAB[((y - 4 + order) * MAXX) + xz] * exp3;
+    //             matC_xyz += matAB[((y - 5 + order) * MAXX) + xz] * exp4;
+    //             matC_xyz += matAB[((y - 6 + order) * MAXX) + xz] * exp5;
+    //             matC_xyz += matAB[((y - 7 + order) * MAXX) + xz] * exp6;
+    //             matC_xyz += matAB[((y - 8 + order) * MAXX) + xz] * exp7;
+
+    //             // Compute all orders on the z axis (first positive direction, then negative one)
+    //             matC_xyz += matAB[((z + 1 + order) * xyplane) + xy] * exp0;
+    //             matC_xyz += matAB[((z + 2 + order) * xyplane) + xy] * exp1;
+    //             matC_xyz += matAB[((z + 3 + order) * xyplane) + xy] * exp2;
+    //             matC_xyz += matAB[((z + 4 + order) * xyplane) + xy] * exp3;
+    //             matC_xyz += matAB[((z + 5 + order) * xyplane) + xy] * exp4;
+    //             matC_xyz += matAB[((z + 6 + order) * xyplane) + xy] * exp5;
+    //             matC_xyz += matAB[((z + 7 + order) * xyplane) + xy] * exp6;
+    //             matC_xyz += matAB[((z + 8 + order) * xyplane) + xy] * exp7;
+    //             matC_xyz += matAB[((z - 1 + order) * xyplane) + xy] * exp0;
+    //             matC_xyz += matAB[((z - 2 + order) * xyplane) + xy] * exp1;
+    //             matC_xyz += matAB[((z - 3 + order) * xyplane) + xy] * exp2;
+    //             matC_xyz += matAB[((z - 4 + order) * xyplane) + xy] * exp3;
+    //             matC_xyz += matAB[((z - 5 + order) * xyplane) + xy] * exp4;
+    //             matC_xyz += matAB[((z - 6 + order) * xyplane) + xy] * exp5;
+    //             matC_xyz += matAB[((z - 7 + order) * xyplane) + xy] * exp6;
+    //             matC_xyz += matAB[((z - 8 + order) * xyplane) + xy] * exp7;
+
+    //             matC[xyz] = matC_xyz;
+    //         }
+    //     }
+    // }
 }
 
 [[nodiscard]] auto main() -> int32_t {
